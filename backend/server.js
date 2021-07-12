29f21c1a5c75b16f164a871066dbe4b243165a2e
kedar <kedar.kanday@gmail.com>
2021-07-12 22:17:15 +0530

    test with get /*

--- backend/server.js
+++ backend/server.js
@@ -9,7 +9,9 @@ const buildPath = path.join(__dirname, '..', 'build');
 app.use(express.static(buildPath));
 const routerURL=require('./route');
 app.use('/v1',routerURL)
-
+app.get('/*',function(req,res){
+    res.sendFile(path.resolve(__dirname,'/build/index.html'));
+})
 app.listen(PORT,()=>{
     console.log("listening at "+PORT);
 })
