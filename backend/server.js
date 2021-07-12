354bb72193eac2be9ac73c8e25f9079a7972a7df
kedar <kedar.kanday@gmail.com>
2021-07-12 20:10:53 +0530

    same port

--- backend/server.js
+++ backend/server.js
@@ -8,15 +8,15 @@ var movies=[{"movie_name":"movie 1","suggested_by":"kedar","votes":1,"link":"lin
 app.get('/movies',(req,res)=>{
     res.send(movies).status(200);
 })
-app.get('/',(req,res)=>{
+app.get('/v1/',(req,res)=>{
     res.send('hi');
 })
-app.post('/movie',(req,res)=>{
+app.post('/v1/movie',(req,res)=>{
     console.log(req.body.movie);
     movies.push(req.body.movie);
     res.send(movies).status(200);
 })
 
-app.listen(9000,()=>{
+app.listen(3000,()=>{
     console.log("listening at 9000");
 })
