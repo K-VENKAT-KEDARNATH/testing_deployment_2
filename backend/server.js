4efaadf1510221ccc8bde436db09e90528e59b99
kedar <kedar.kanday@gmail.com>
2021-07-16 17:24:13 +0530

    commit

--- backend/server.js
+++ backend/server.js
@@ -6,7 +6,7 @@ const app=express();
 var cors=require('cors')
 app.use(cors())
 app.use(express.json())
-const PORT=process.env.PORT || 3000;
+const PORT=process.env.PORT || 5000;
 const buildPath = path.join(__dirname, '..', 'build');
 app.use(express.static(buildPath));
 const routerURL=require('./route');
